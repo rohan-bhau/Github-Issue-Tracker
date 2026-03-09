38462283ec04f4eecfa33bdfb67e3d9e8cb2fecd
Rohan Mia <rohanmia.org@gmail.com>
2026-03-09 20:25:59 +0600

    created navbar

--- js/login.js
+++ js/login.js
@@ -1,16 +1,19 @@
-const loginBtn = document.getElementById("login-btn").addEventListener("click", function(){
+const loginBtn = document
+  .getElementById("login-btn")
+  .addEventListener("click", function () {
     //? 1. get the username
-    const username = document.getElementById('username').value
-    console.log(username)
+    const username = document.getElementById("username").value;
+    console.log(username);
 
     //? 2. get the password
-    const password = document.getElementById("password").value
-    console.log(password)
+    const password = document.getElementById("password").value;
+    console.log(password);
 
-    if(username === "admin" && password==="admin123"){
-        alert("Logged In Successful")
-        window.location.assign("main.html")
-    }else{
-        alert("Invalid Credentials")
+    if (username === "admin" && password === "admin123") {
+      alert("Logged In Successful");
+      window.location.assign("main.html");
+    } else {
+      alert("Invalid Credentials");
+      return;
     }
-})
+  });
